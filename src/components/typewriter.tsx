5721d215a124f5acda8ed7ff2380625e52199bdc
Ali <44374885+alboainin@users.noreply.github.com>
2024-07-20 06:11:54 +0300

    Fixed pfp loading error

--- src/components/typewriter.tsx
+++ src/components/typewriter.tsx
@@ -1,45 +1,50 @@
-import React, { useState, useEffect } from 'react';
+"use client";
+
+import React, { useEffect, useState } from "react";
+import "./typewriter.css";
 
 const Typewriter: React.FC = () => {
-  const [text, setText] = useState('');
-  const [loopNum, setLoopNum] = useState(0);
+  const [text, setText] = useState("");
   const [isDeleting, setIsDeleting] = useState(false);
+  const [loopNum, setLoopNum] = useState(0);
+  const [typingSpeed, setTypingSpeed] = useState(150);
+
   const textArray = [
-    "What do you call an alligator wearing a vest?|An Investigator", 
-    "What do you call a fake noodle?|An Impasta", 
-    "Why shouldn&apos;t you write with a broken pencil?|Because it&apos;s pointless",
-    "Why couldn&apos;t the pirate finish the alphabet?|He kept getting lost at C",
-    "What&apos;s brown and sticky?|A stick",
-    "What starts with an E, ends with an E and has one letter in it?|An Envelope",
-    "What has four wheels, and flies?|A Garbage truck",
-    "What do you call a pig that knows Karate?|Pork Chop",
-    "Why did the scarecrow get promoted?|He was out standing in his field.",
-    "I have a step ladder|I never knew my real ladder.",
-    "What kind of shoes do ninjas wear?|Sneakers"
+    "Developer",
+    "Open-source maintener",
+    "Building startups for fun",
+    "Hobbiest Guitarist",
+    "Go Warriors!"
   ];
 
   useEffect(() => {
+    let timer: NodeJS.Timeout;
     const handleType = () => {
-      const i = loopNum % textArray.length;
-      const fullText = textArray[i];
-      const updatedText = isDeleting
-        ? fullText.substring(0, text.length - 1)
-        : fullText.substring(0, text.length + 1);
+      const current = loopNum % textArray.length;
+      const fullText = textArray[current];
 
-      setText(updatedText);
+      setText(
+        isDeleting
+          ? fullText.substring(0, text.length - 1)
+          : fullText.substring(0, text.length + 1)
+      );
 
-      if (!isDeleting && updatedText === fullText) {
+      setTypingSpeed(isDeleting ? 35 : 150);
+
+      if (!isDeleting && text === fullText) {
         setTimeout(() => setIsDeleting(true), 500);
-      } else if (isDeleting && updatedText === '') {
+      } else if (isDeleting && text === "") {
         setIsDeleting(false);
         setLoopNum(loopNum + 1);
-      } else {
-        setTimeout(handleType, isDeleting ? 100 : 200);
       }
+
+      timer = setTimeout(handleType, typingSpeed);
     };
 
-    handleType();
-  }, [isDeleting, loopNum, textArray]); // Added missing dependencies
+    timer = setTimeout(handleType, typingSpeed);
+
+    return () => clearTimeout(timer);
+  }, [text, isDeleting, typingSpeed]);
 
   return (
     <div className="container py-5">
